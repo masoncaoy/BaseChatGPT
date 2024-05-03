3d005a99b3978a796dba66c7af908a54cb5980a6
masoncao <masoncaoy@gmail.com>
2024-05-03 11:54:13 -0700

    Initial Commit

---
+++ BaseChatGPT/BaseChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseChatGPTApp.swift
+//  BaseChatGPT
+//
+//  Created by Weifeng Cao on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BaseChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
